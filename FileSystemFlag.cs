af35004195440b0e45adbf79d182d7e2ccb8c497
AlexeyChumakov2001 <AlexeyChumakov2001@gmail.com>
2021-09-02 09:04:54 +0300

    Complited task 1

---
+++ FileSystemFlag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Advanced_CSharp
+{
+	[Flags]
+	public enum FileSystemFlag
+	{
+		None = 0,
+		Directory = 1,
+		File = 2,
+		TxtFile = 4,
+		LongNameFile = 8,
+		LongNameDirectory = 16
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace Advanced_CSharp
+{
+	class Program
+	{
+		static void Main()
+		{
+			string startDirectory = @"C:\Program Files";
+
+			FileSystemVisitor fileSystemVisitor = new FileSystemVisitor(startDirectory, file => file.Name == "IIS",  FileSystemFlag.TxtFile, FileSystemFlag.LongNameDirectory | FileSystemFlag.LongNameFile);
+
+			fileSystemVisitor.Start += (directory => Console.WriteLine($"Searching has been started in directory \"{directory}\"\n"));
+			fileSystemVisitor.Finish += (directory => Console.WriteLine($"\nSearching has been finished in directory \"{directory}\"")); ;
+
+			fileSystemVisitor.FileFinded += ((file, stopFlag, ignoreFlag) =>
+			{
+				if(!(ignoreFlag.HasFlag(FileSystemFlag.File)
+					 || (ignoreFlag.HasFlag(FileSystemFlag.LongNameFile) && file.Name.Length > 14)
+					 || (ignoreFlag.HasFlag(FileSystemFlag.TxtFile) && file.Extension == ".txt")))
+				{
+					Console.WriteLine($"\"{file.Name}\" file was found");
+				}
+
+				if (stopFlag.HasFlag(FileSystemFlag.File)
+				      || (stopFlag.HasFlag(FileSystemFlag.LongNameFile) && file.Name.Length > 14)
+				      || (stopFlag.HasFlag(FileSystemFlag.TxtFile) && file.Extension == ".txt"))
+				{
+					Console.WriteLine("Stop searching!");
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			});
+			fileSystemVisitor.DirectoryFinded += ((directory, stopFlag, ignoreFlag) =>
+			{
+				if (!(ignoreFlag.HasFlag(FileSystemFlag.Directory)
+				      || (ignoreFlag.HasFlag(FileSystemFlag.LongNameDirectory) && directory.Name.Length > 10)))
+				{
+					Console.WriteLine($"\"{directory.Name}\" directory was found");
+				}
+
+				if (stopFlag.HasFlag(FileSystemFlag.Directory)
+				      || (stopFlag.HasFlag(FileSystemFlag.LongNameDirectory) && directory.Name.Length > 10))
+				{
+					Console.WriteLine("Stop searching!");
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			});
+
+			fileSystemVisitor.FilteredFileFinded += ((file, stopFlag, ignoreFlag) =>
+			{
+				if (!(ignoreFlag.HasFlag(FileSystemFlag.File)
+				      || (ignoreFlag.HasFlag(FileSystemFlag.LongNameFile) && file.Name.Length > 14)
+				      || (ignoreFlag.HasFlag(FileSystemFlag.TxtFile) && file.Extension == ".txt")))
+				{
+					Console.BackgroundColor = ConsoleColor.Green;
+					Console.ForegroundColor = ConsoleColor.Black;
+
+					Console.WriteLine(
+						$"\"{file.Name}\" file was found and matched by filter");
+
+					Console.BackgroundColor = ConsoleColor.Black;
+					Console.ForegroundColor = ConsoleColor.White;
+				}
+
+				if (stopFlag.HasFlag(FileSystemFlag.File)
+				      || (stopFlag.HasFlag(FileSystemFlag.LongNameFile) && file.Name.Length > 14)
+				      || (stopFlag.HasFlag(FileSystemFlag.TxtFile) && file.Extension == ".txt"))
+				{
+					Console.WriteLine("Stop searching!");
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			});
+			fileSystemVisitor.FilteredDirectoryFinded += ((directory, stopFlag, ignoreFlag) =>
+			{
+				if (!(ignoreFlag.HasFlag(FileSystemFlag.Directory)
+				      || (ignoreFlag.HasFlag(FileSystemFlag.LongNameDirectory) && directory.Name.Length > 10)))
+				{
+					Console.BackgroundColor = ConsoleColor.Green;
+					Console.ForegroundColor = ConsoleColor.Black;
+
+					Console.WriteLine(
+						$"\"{directory.Name}\" directory was found and matched by filter");
+
+					Console.BackgroundColor = ConsoleColor.Black;
+					Console.ForegroundColor = ConsoleColor.White;
+				}
+
+				if (stopFlag.HasFlag(FileSystemFlag.Directory)
+				      || (stopFlag.HasFlag(FileSystemFlag.LongNameDirectory) && directory.Name.Length > 10))
+				{
+					Console.WriteLine("Stop searching!");
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			});
+
+			foreach (var file in fileSystemVisitor)
+			{ 
+			}
+		}
+	}
+}
